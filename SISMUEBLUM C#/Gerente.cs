d9553328c75ea8571fc7983fd7b086a1c8604751
JorgexDxDxD <jorgegabriel98@hotmail.com>
2018-04-10 14:17:55 -0500

    cs Jorge

---
+++ SISMUEBLUM C#/Gerente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    class Gerente : Usuario
+    {
+
+        public Gerente(string nombre, string apellidoPaterno, string apellidoMaterno, string dni, string username, string password, 
+            bool prioridad) : base(nombre, apellidoPaterno, apellidoMaterno, dni, username, password, prioridad)
+        {
+        }
+
+        public void consultarVendedoresSede(string sede)
+        {
+
+        }
+       
+        public void asignarVendedorSede(Vendedor v,string sede)
+        {
+
+        }
+    }
+}
