27c76c1456227da00e8212ce77f1b0eb52c45b49
JosePucp011 <38263090+JosePucp011@users.noreply.github.com>
2018-04-10 17:09:56 -0500

    actualizacion

--- SISMUEBLUM C#/almacen.cs
+++ SISMUEBLUM C#/almacen.cs
@@ -1,7 +1,5 @@
 public class Almacen {
 
-	private string direccion;
-	private int telefono;
 	private List<Productos> listaStockProd;
 	private List<int>  listaStockProdInt;
 
@@ -9,24 +7,23 @@ public class Almacen {
 		listaStockProd = new ArrayList<Productos>();		
 		listaStockProdInt = new ArrayList<int>();
 	}
-
-	public string Direccion{
-		get{
-			return _direccion;
-		}
-		set{
-			_direccion = value;
-		}
+	
+	public void mostrarAlmacen(){
+	
 	}
 	
-	public int Telefono{
-		get{
-			return _telefono;
-		}
-		set{
-			_telefono = value;
-		}
+	puclic void registrarProducto(producto prod, int cantidad){
+	
 	}
+	
+	public void sacarProducto(Producto p, int cant){
+	
+	}
+	
+	public void enviarAExhibicion(listaStockProd list,listaStockProdInt c){
+	
+	}
+	
 	
 	
 
